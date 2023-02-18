bada78e9c862283c66ad1d0ad5ddb44c6351a398
camarer0 <perovAndrew2003@gmail.com>
2023-02-18 19:29:15 +0300

    more than one examples in input.txt

--- Calc/src/Calculator.java
+++ Calc/src/Calculator.java
@@ -2,75 +2,85 @@ import java.io.*;
 import java.util.Scanner;
 
 class Calculator {
-    static public void main (String []args) throws FileNotFoundException {
+    static public void main(String[] args) throws FileNotFoundException {
         File file = new File("C:\\Users\\perov\\IdeaProjects\\stepik_calc\\Calc\\src\\input.txt");
-        Scanner sc = new Scanner (file);
-        String line = sc.nextLine();
-        sc.close();
+        Scanner sc = new Scanner(file);
         double a = 0.0;
         double b = 0.0;
         char znak = ' ';
-        int m=0;
+        int m = 0;
         boolean is = true;
-        for(String num:line.split(" ")) {
-            m++;
-            if(m==1) {
-                try {
-                    a = Double.parseDouble(num);
-                }catch(NumberFormatException e) {
-                    System.out.print("Error! Not number\n");
-                    is = false;
-                    break;
-                }
-            }
-            if(m==3) {
-                try {
-                    b = Double.parseDouble(num);
-                }catch(NumberFormatException e) {
-                    System.out.print("Error! Not number\n");
-                    is = false;
-                    break;
-                }
-            }
-        }
-        if(is) {
-            m=0;
-            for(String num:line.split(" ")) {
+        while (sc.hasNextLine()) {
+            String line = sc.nextLine();
+            for (String num : line.split(" ")) {
                 m++;
-                if(m==2) {
+                if (m == 1) {
                     try {
-                        znak = result(num);
-                    }catch(Exception s){
-                        System.out.print("Operation Error!");
-                        is=false;
+                        a = Double.parseDouble(num);
+                    } catch (NumberFormatException e) {
+                        System.out.print("Error! Not number\n");
+                        is = false;
+                        break;
+                    }
+                }
+                if (m == 3) {
+                    try {
+                        b = Double.parseDouble(num);
+                    } catch (NumberFormatException e) {
+                        System.out.print("Error! Not number\n");
+                        is = false;
                         break;
                     }
                 }
             }
-        }
-        if(is) {
-            double res = 0;
-            switch (znak) {
-                case ('+'): System.out.print(a + b); res=a+b; break;
-                case ('-'):System.out.print(a - b); res=a-b; break;
-                case ('/'):{
-                    if (b == 0.0) System.out.print("Error! Division by zero");
-                    else {
-                        System.out.print(a / b);
-                        res=a/b;
-                        break;
+            if (is) {
+                m = 0;
+                for (String num : line.split(" ")) {
+                    m++;
+                    if (m == 2) {
+                        try {
+                            znak = result(num);
+                        } catch (Exception s) {
+                            System.out.print("Operation Error!");
+                            is = false;
+                            break;
+                        }
                     }
                 }
-                case ('*'):System.out.print(a * b); res=a*b; break;
             }
-            try(FileWriter writer = new FileWriter("C:\\Users\\perov\\IdeaProjects\\stepik_calc\\Calc\\src\\output.txt", true)){
-                writer.write(res+"");
-            }
-            catch(IOException ex){
+            if (is) {
+                double res = 0;
+                switch (znak) {
+                    case ('+'):
+                        System.out.println(a + b);
+                        res = a + b;
+                        break;
+                    case ('-'):
+                        System.out.println(a - b);
+                        res = a - b;
+                        break;
+                    case ('/'): {
+                        if (b == 0.0) System.out.print("Error! Division by zero");
+                        else {
+                            System.out.println(a / b);
+                            res = a / b;
+                            break;
+                        }
+                    }
+                    case ('*'):
+                        System.out.println(a * b);
+                        res = a * b;
+                        break;
+                }
+                try (FileWriter writer = new FileWriter("C:\\Users\\perov\\IdeaProjects\\stepik_calc\\Calc\\src\\output.txt", true)) {
+                    writer.write(line + " = " + res + "\n");
+                } catch (IOException ex) {
 
-                System.out.println(ex.getMessage());
+                    System.out.println(ex.getMessage());
+                }
             }
         }
+        sc.close();
     }
     public static char result(String nms) throws Exception {
         if(nms.equals("/") || nms.equals("*") || nms.equals("+") || nms.equals("-")) {
